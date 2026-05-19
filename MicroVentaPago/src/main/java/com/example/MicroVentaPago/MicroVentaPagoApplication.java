417981b5bfe866f1b1164d5f035ad9644633ee8a
dieriverar28 <die.riverar@duocuc.cl>
2026-05-19 12:07:16 -0400

    Add files via upload

---
+++ MicroVentaPago/src/main/java/com/example/MicroVentaPago/MicroVentaPagoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroVentaPago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroVentaPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroVentaPagoApplication.class, args);
+	}
+
+}
---
+++ MicroVentaPago/src/test/java/com/example/MicroVentaPago/MicroVentaPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroVentaPago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroVentaPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
